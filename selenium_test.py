b031cb2cd0b48ce54983edec99d78b5534e5b64b
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-29 21:24:27 +0300

    Summary

--- selenium_test.py
+++ selenium_test.py
@@ -28,5 +28,5 @@ def get_whole_page(page_link: str, filename: str):
 
 
 
-example = get_whole_page('http://honcharenkozhenya.github.io/my-projects/kinomonster-bootstrap/index.html', 'FILE.HTML')
+example = get_whole_page('http://projects.valyakin.ru/Kinomonster-Bootstrap/show.html', 'FILE.HTML')
 print(example)
