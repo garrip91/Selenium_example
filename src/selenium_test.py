6ca4d77a096581c531548625fa70a0179c9afe37
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-25 11:12:07 +0300

    Summary

---
+++ src/selenium_test.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from time import sleep
+
+
+def get_whole_page(page_link: str, filename: str):
+    
+    with webdriver.Chrome() as chrome:
+        
+        chrome.get(page_link)
+        
+        # Берём вертикальное положение прокрутки страницы:
+        offset = chrome.execute_script("return window.pageYOffset;")
+        while True:
+            # Дальше мы прокручиваем до самого конца:
+            chrome.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            # Задержка в секундах, в течение которой страница подгрузится:
+            sleep(3)
+            # Если есть возможность прокручивать дальше, то мы прокручиваем:
+            if (new_offset := chrome.execute_script("return window.pageYOffset;")) != offset:
+                offset = new_offset
+            else:
+                break
+
+        with open(filename, 'w', encoding='UTF-8') as file:
+            file.write(chrome.page_source)
+		
+        chrome.quit()
+
+
+
+example = get_whole_page('https://letto-interior.ru/', 'main.html')
+print(example)
